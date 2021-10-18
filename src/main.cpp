ab439f2cdb3bcd702805313fac1878562f268e1b
Strooom <pascal.roobrouck@gmail.com>
2021-10-18 21:45:05 +0200

    Added displayValuesTimer

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,51 @@
 #include <Arduino.h>
 #include "runtimer.h"
-#include "logging.h"
+
 
 #define ledPin 10
-#define photoResistorPin A1
+#define photoResistorPin A0
 
-constexpr uint32_t brightnessIncrement{10U};
-constexpr uint32_t brightnessMax{120U};
+intervalTimer writeOutputTimer(100U);
+intervalTimer readInputTimer(25U);
+intervalTimer displayValuesTimer(500U);
 
-intervalTimer writeOutputTimer(250U);
-uLog theLog(loggingLevel::Debug, true);
+constexpr uint8_t nmbrSamples{8U};
+uint32_t ambientLight[nmbrSamples];
+uint8_t sampleIndex{0};
 uint32_t brightness{0};
 
 void setup() {
     delay(200U);
     Serial.begin(115200U);
     Serial.flush();
-    theLog.setOutputIsAvailable(true);
 
     pinMode(ledPin, OUTPUT);
     pinMode(photoResistorPin, INPUT);
 }
 
 void loop() {
+    if (readInputTimer.expired()) {
+        ambientLight[sampleIndex] = analogRead(photoResistorPin);
+        sampleIndex               = (sampleIndex + 1) % nmbrSamples;
+    }
+
     if (writeOutputTimer.expired()) {
-        theLog.snprintf(loggingLevel::Debug, "brightness = %d", brightness);
+        uint32_t totalAmbientLight{0};
+        for (int i = 0; i < nmbrSamples; i++) {
+            totalAmbientLight = totalAmbientLight + ambientLight[i];
+        }
+        uint32_t averageAmbientLight = totalAmbientLight / (nmbrSamples * 4);
+        if (averageAmbientLight > 255) {
+            averageAmbientLight = 255;
+        }
+        brightness = 255 - averageAmbientLight;
         analogWrite(ledPin, brightness);
-        brightness = ((brightness + brightnessIncrement) % brightnessMax);
+    }
+
+    if (displayValuesTimer.expired()) {
+        Serial.print("ambient = ");
+        Serial.print(ambientLight[0]);
+        Serial.print(" | brightness = ");
+        Serial.println(brightness);
     }
 }
